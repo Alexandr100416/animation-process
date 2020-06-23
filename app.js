01da99a27d21f234b32a0488b6f909d22b51946e
Александр Медолиз <aleksandrmedoliz@MacBook-Pro-Aleksandr.local>
2020-06-23 22:11:06 +0300

    added firs picture

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
-PIXI.utils.sayHello();
+let Application = PIXI.Application;
+let loader = PIXI.loader;
+let resources = PIXI.loader.resources;
+let Sprite = PIXI.Sprite;
 
 
-const app = new PIXI.Application({
-    width: 800, height: 600, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
-});
+
+let app = new Application({ 
+    width: 256, 
+    height: 256,                       
+    antialias: true, 
+    transparent: false, 
+    resolution: 1
+  }
+);
+
+//Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
 
-const container = new PIXI.Container();
+//load an image and run the `setup` function when it's done
+loader
+  .add("catImage", "/cat.png")
+  .load(setup);
 
-app.stage.addChild(container);
+//This `setup` function will run when the image has loaded
+function setup() {
 
-// Create a new texture
-const texture = PIXI.Texture.from('examples/assets/bunny.png');
-
-// Create a 5x5 grid of bunnies
-for (let i = 0; i < 25; i++) {
-    const bunny = new PIXI.Sprite(texture);
-    bunny.anchor.set(0.5);
-    bunny.x = (i % 5) * 40;
-    bunny.y = Math.floor(i / 5) * 40;
-    container.addChild(bunny);
+  //Create the cat sprite
+  let cat = new Sprite(resources["catImage"].texture);
+  
+  //Add the cat to the stage
+  app.stage.addChild(cat);
 }
 
-// Move container to the center
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
-
-// Center bunny sprite in local container coordinates
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
-
-// Listen for animate update
-app.ticker.add((delta) => {
-    // rotate the container!
-    // use delta to create frame-independent transform
-    container.rotation -= 0.01 * delta;
-});
