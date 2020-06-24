9e125b5a964c6e7e6ce87c480a8c684ae738f60e
Александр Медолиз <aleksandrmedoliz@MacBook-Pro-Aleksandr.local>
2020-06-24 15:28:15 +0300

    clear code

--- app.js
+++ app.js
@@ -9,29 +9,20 @@ class Application {
     document.body.appendChild(this.renderer.view);
     this.ticker = new PIXI.Ticker();
     this.stage = new PIXI.Container();
-
-    this.ticker .add(this.render.bind(this), PIXI.UPDATE_PRIORITY.LOW);
-this.ticker.start();
-}
-    
-    get screen (){
-        return this.renderer.screen;
-    }
-  render (){
+    this.ticker.add(this.render.bind(this), PIXI.UPDATE_PRIORITY.LOW);
+    this.ticker.start();
+  }
+  get screen() {
+    return this.renderer.screen;
+  }
+  render() {
     this.renderer.render(this.stage);
+  }
 }
-}
-const app = window.app = new Application();
-
-
+const app = (window.app = new Application());
 const container = new PIXI.Container();
-
 app.stage.addChild(container);
-
-// Create a new texture
 const texture = PIXI.Texture.from("./img/beaver.svg");
-
-// Create a 5x5 grid of bunnies
 for (let i = 0; i < 25; i++) {
   const bunny = new PIXI.Sprite(texture);
   bunny.anchor.set(0.5);
@@ -41,19 +32,10 @@ for (let i = 0; i < 25; i++) {
   bunny.height = 35;
   container.addChild(bunny);
 }
-
-// Move container to the center
 container.x = app.screen.width / 2;
 container.y = app.screen.height / 2;
-
-// Center bunny sprite in local container coordinates
 container.pivot.x = container.width / 2;
 container.pivot.y = container.height / 2;
-
-// Listen for animate update
 app.ticker.add((delta) => {
-  // rotate the container!
-  // use delta to create frame-independent transform
   container.rotation -= 0.01 * delta;
 });
-
