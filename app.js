d65569a7e59da1ab25ea49be4be68223ad2e3baa
Александр Медолиз <aleksandrmedoliz@MacBook-Pro-Aleksandr.local>
2020-06-24 18:01:25 +0300

    added cursor movie with element

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-class Application {
-  constructor() {
-    this.renderer = new PIXI.Renderer({
-      width: window.innerWidth,
-      height: window.innerHeight,
-      backgroundColor: 0x1099bb,
-      resolution: 1,
-    });
-    document.body.appendChild(this.renderer.view);
-    this.ticker = new PIXI.Ticker();
-    this.stage = new PIXI.Container();
-    this.ticker.add(this.render.bind(this), PIXI.UPDATE_PRIORITY.LOW);
-    this.ticker.start();
-  }
-  get screen() {
-    return this.renderer.screen;
-  }
-  render() {
-    this.renderer.render(this.stage);
-  }
-}
-const app = (window.app = new Application());
-const container = new PIXI.Container();
-app.stage.addChild(container);
-const texture = PIXI.Texture.from("./img/beaver.svg");
-for (let i = 0; i < 25; i++) {
-  const bunny = new PIXI.Sprite(texture);
-  bunny.anchor.set(0.5);
-  bunny.x = (i % 5) * 40;
-  bunny.y = Math.floor(i / 5) * 40;
-  bunny.width = 35;
-  bunny.height = 35;
-  container.addChild(bunny);
-}
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
-app.ticker.add((delta) => {
-  container.rotation -= 0.01 * delta;
+const app = new PIXI.Application({
+  width: 800,
+  height: 600,
+  backgroundColor: 0x1099bb,
+  resolution: window.devicePixelRatio || 1,
 });
+document.body.appendChild(app.view);
+const texture = PIXI.Texture.from("./img/beaver.svg");
+createBunny(40, 40);
+function createBunny(x, y) {
+  const bunny = new PIXI.Sprite(texture);
+  bunny.width = 50;
+  bunny.height = 50;
+  bunny.interactive = true;
+  bunny.buttonMode = true;
+  bunny.anchor.set(0.5);
+  bunny.scale.set(3);
+  bunny
+    .on("pointerdown", onDragStart)
+    .on("pointerup", onDragEnd)
+    .on("pointerupoutside", onDragEnd)
+    .on("pointermove", onDragMove);
+  bunny.x = x;
+  bunny.y = y;
+  app.stage.addChild(bunny);
+}
+function onDragStart(event) {
+  this.data = event.data;
+  this.alpha = 0.5;
+  this.dragging = true;
+}
+function onDragEnd() {
+  this.alpha = 1;
+  this.dragging = false;
+
+  this.data = null;
+}
+function onDragMove() {
+  if (this.dragging) {
+    const newPosition = this.data.getLocalPosition(this.parent);
+    this.x = newPosition.x;
+    this.y = newPosition.y;
+  }
+}
